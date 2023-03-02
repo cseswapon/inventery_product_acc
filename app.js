6f0df660446337c5bfaad26ca3ceaa7107a54978
aotrek <otatbpdevelopment@gmail.com>
2023-03-02 13:10:32 +0600

    Added my new feature

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const productRoute = require("./routes/product.route");
-const brandRoute = require('./routes/brand.route');
-const storeRoute = require('./routes/store.route')
+const brandRoute = require("./routes/brand.route");
+const storeRoute = require("./routes/store.route");
 const categoryRoute = require("./routes/category.route");
+const userRoute = require("./routes/user.route");
+const loinUser = require("./routes/login.route");
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/brand", brandRoute);
 app.use("/api/v1/store", storeRoute);
-app.use("/api/v1/brand", categoryRoute);
+// app.use("/api/v1/brand", categoryRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/user", loinUser);
 
 // root route
 app.get("/", (req, res) => {
